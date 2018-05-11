00843824bf61ee4ad2c90ed0ecf14fa69efc55eb
cuteprettykara <@gmail.com>
2018-05-11 10:36:34 +0900

    P6_1_인터셉터를 활용하는 로그인 처리(Spring MVC의 Interceptor 알아보기)

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,11 +1,14 @@
 package org.zerock.controller;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.ProductVO;
@@ -16,13 +19,18 @@ public class SampleController {
 	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
 	@RequestMapping("doA")
-	public void doA() {
+	public String doA(Locale locale, Model model) {
 		logger.info("doA called.......................");
+		
+		return "home";
 	}
 	
-	@RequestMapping("doB")
-	public void doB() {
+	@RequestMapping(value="doB", method=RequestMethod.GET)
+	public String doB(Locale locale, Model model) {
 		logger.info("doB called.......................");
+		
+		model.addAttribute("result", "DOB RESULT");
+		return "home";
 	}
 	
 	@RequestMapping("doC")
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ page session="false"%>
 
 <!DOCTYPE html>
 <html>
@@ -32,7 +31,8 @@
 				</div>
 				
 				<div class="box-body">
-					Start creating your amazing application!
+					Start creating your amazing application!<br>
+					${result}
 				</div>
 				<!-- /.box-body -->
 				
